04653fc4d5a4fd3a7f176d9e239db5efd1af3a12
Filipe Silva <filipe.silva@yacoobalabs.com>
2024-07-21 12:54:18 +0100

    feat: added images description

--- src/components/molecules/SwiperCard.tsx
+++ src/components/molecules/SwiperCard.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, View } from 'react-native'
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
 
 export type SwiperCardProps = {
 	title: string
@@ -7,10 +7,26 @@ export type SwiperCardProps = {
 	description: string
 }
 
-const SwiperCard = ({ imageURL }: SwiperCardProps) => {
+const SwiperCard = ({
+	imageURL,
+	description,
+	stats,
+	title,
+}: SwiperCardProps) => {
 	return (
 		<View style={styles.mainContainer}>
-			<Image style={styles.image} source={{ uri: imageURL }} />
+			<View style={styles.imageContainer}>
+				<Image style={styles.image} source={{ uri: imageURL }} />
+				<View style={styles.detailsContainer}>
+					<View style={styles.titleRow}>
+						<Text style={styles.titleText}>{title}</Text>
+						<Text style={styles.titleText}>{stats}</Text>
+					</View>
+					<View>
+						<Text style={styles.descriptionText}>{description}</Text>
+					</View>
+				</View>
+			</View>
 		</View>
 	)
 }
@@ -21,10 +37,51 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
-	image: {
-		width: '80%',
-		height: '60%',
+	imageContainer: {
+		height: Dimensions.get('window').height * 0.6,
+		width: Dimensions.get('window').width * 0.9,
 		borderRadius: 20,
+		justifyContent: 'center',
+		alignItems: 'center',
+		shadowColor: '#000000d1',
+		shadowOffset: {
+			width: 0,
+			height: 10,
+		},
+		shadowOpacity: 0.51,
+		shadowRadius: 13.16,
+		elevation: 20,
+	},
+	image: {
+		width: '100%',
+		height: '100%',
+		borderRadius: 20,
+	},
+	detailsContainer: {
+		height: 48,
+		width: '90%',
+		padding: 10,
+		zIndex: 100,
+		bottom: 0,
+		alignSelf: 'center',
+		position: 'absolute',
+		backgroundColor: 'white',
+		borderTopRightRadius: 20,
+		borderTopLeftRadius: 20,
+	},
+	titleRow: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	titleText: {
+		fontSize: 20,
+		fontWeight: '700',
+		color: '#434141',
+	},
+	descriptionText: {
+		fontSize: 10,
+		fontWeight: '700',
+		color: '#BFBFC0',
 	},
 })
 
