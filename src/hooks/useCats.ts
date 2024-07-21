4e9228fb0aac2f06695a55295fcc1d32fb09787b
Filipe Silva <filipe.silva@yacoobalabs.com>
2024-07-21 11:48:48 +0100

    chore: added pagination method

--- src/hooks/useCats.ts
+++ src/hooks/useCats.ts
@@ -1,10 +1,11 @@
 import useSWRInfinite from 'swr/infinite'
 import { theCatAPI } from '../api/catApi/catApi'
+import { useCallback } from 'react'
 
 const useCats = () => {
-	const fetcher = async (page: number) => {
+	const fetcher = (page: number) => {
 		try {
-			return await theCatAPI.images.searchImages({
+			return theCatAPI.images.searchImages({
 				page,
 				limit: 10,
 				size: 'small',
@@ -17,14 +18,36 @@ const useCats = () => {
 		}
 	}
 
-	const { setSize, isLoading, data, error } = useSWRInfinite(
+	const { setSize, isLoading, data, error, isValidating } = useSWRInfinite(
 		page => ['searchImages', page],
-		async ([_, page]) => fetcher(page),
+		([_, page]) => fetcher(page),
 		{ parallel: true },
 	)
+
 	const catListData = data?.flat() ?? []
 
-	return { isLoading, catListData, error, setSize }
+	const loadPagination = useCallback(
+		(swiperIndex: number) => {
+			if (
+				!isLoading &&
+				!isValidating &&
+				catListData &&
+				catListData.length > 0 &&
+				swiperIndex >= catListData.length - 6
+			) {
+				setSize(size => size + 1)
+			}
+		},
+		[isLoading, catListData?.length, catListData],
+	)
+
+	return {
+		isLoading,
+		isValidating,
+		catListData,
+		error,
+		loadPagination,
+	}
 }
 
 export default useCats
